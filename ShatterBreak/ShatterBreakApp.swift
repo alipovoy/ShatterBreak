446ee8834d2cc4c7dae5f13f309cd85c79eb0623
Alexey Lipovoy <aleksey.lipovoy@gmail.com>
2026-02-26 19:40:01 -0800

    Initial Commit

---
+++ ShatterBreak/ShatterBreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShatterBreakApp.swift
+//  ShatterBreak
+//
+//  Created by Alexey Lipovoy on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct ShatterBreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
